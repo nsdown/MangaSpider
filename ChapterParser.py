1612cfc10b19547bb862469ea08d0e0744bb3003
juiceyang <abandonclock@gmail.com>
2015-10-23 17:29:46 +0800

    try decouple classes

--- ChapterParser.py
+++ ChapterParser.py
@@ -2,23 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+
 class ChapterParser:
-    def __init__(self, mangaurl):
-        self.__url = mangaurl
-        self.chList = []
+    def __init__(self):
+        self.__url = None
+        self.__chList = []
         return
+
+    def GetWork(self, mangaUrl):
+        self.__url = mangaUrl
 
     def GetChapterList(self):
         resp = requests.get(self.__url)
         soup = BeautifulSoup(resp.content, "html.parser")
-        chListCols = soup.find_all("ul", attrs={"class":"nr6 lan2", "id":re.compile("^cbc_\d$")})
+        chListCols = soup.find_all("ul", attrs={"class": "nr6 lan2", "id": re.compile("^cbc_\d$")})
         no = 0
         for col in chListCols:
-            oneCol = col.find_all("a", attrs={"class":"tg"})
+            oneCol = col.find_all("a", attrs={"class": "tg"})
             for ch in oneCol:
-                self.chList.append((no, ch.get("href"), ch.get("title")))
-                no+=1
+                self.__chList.append((no, ch.get("href"), ch.get("title")))
+                no += 1
+        return self.__chList
 
     def PrintAllHref(self):
-        for a in self.chList:
+        for a in self.__chList:
             print a[0], a[1], a[2]
