c7912a159643adc9d8903ec75ab8a2ee8084a3e6
juiceyang <abandonclock@gmail.com>
2015-10-23 15:57:03 +0800

    add #No and title attr to href of chapters

--- ChapterParser.py
+++ ChapterParser.py
@@ -4,19 +4,21 @@ import re
 
 class ChapterParser:
     def __init__(self, mangaurl):
-        self.url = mangaurl
-        self.chDict = {}
+        self.__url = mangaurl
+        self.chList = []
         return
 
     def GetChapterList(self):
-        resp = requests.get(self.url)
+        resp = requests.get(self.__url)
         soup = BeautifulSoup(resp.content, "html.parser")
         chListCols = soup.find_all("ul", attrs={"class":"nr6 lan2", "id":re.compile("^cbc_\d$")})
+        no = 0
         for col in chListCols:
             oneCol = col.find_all("a", attrs={"class":"tg"})
             for ch in oneCol:
-                self.chDict[ch.get("href")] = ch.get("title")
+                self.chList.append((no, ch.get("href"), ch.get("title")))
+                no+=1
 
     def PrintAllHref(self):
-        for a in self.chDict:
-            print a, self.chDict[a]
+        for a in self.chList:
+            print a[0], a[1], a[2]
