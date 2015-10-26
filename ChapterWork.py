f9a9ced6e1bf60589ff7b27ea85c6597928a1d7b
juiceyang <abandonclock@gmail.com>
2015-10-26 17:00:20 +0800

    modify former dict containing work to a queue name pdf files using actual chapter name instead of cid

---
+++ ChapterWork.py
@@ -0,0 +1,17 @@
+from Queue import Queue
+import os
+
+class ChapterWork:
+    def __init__(self, no, chName, chPath):
+        self.chName = chName
+        self.chPath = chPath
+        self.no = no
+        self.imgQueue = Queue()
+        cwd = os.getcwd()
+        self.downloadDir = cwd + "\\" + self.chPath.split('m')[-1]
+        self.outputDir = os.path.abspath(os.path.join(self.downloadDir, os.pardir))
+
+    def PutIntoImgQueue(self, imgWork):
+        # imgwork structure: (imgurl, page)
+        self.imgQueue.put(imgWork)
+        return
