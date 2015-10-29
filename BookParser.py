7511681ce2fcac2baade5e5966035398d6d53498
juiceyang <abandonclock@gmail.com>
2015-10-29 16:07:58 +0800

    delete printallhref method add proxy to bookparser to avoid IP detection

--- BookParser.py
+++ BookParser.py
@@ -2,30 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from ChapterWork import ChapterWork
+import sys
 
 
 class BookParser:
-    def __init__(self, taskQueue):
+    def __init__(self, taskQueue, proxy):
         self.__url = None
-        self.__chList = []
         self.tasker = taskQueue
+        self.__proxy = proxy
         return
 
     def GetWork(self, mangaUrl):
         self.__url = mangaUrl
 
     def Work(self):
-        resp = requests.get(self.__url)
+        resp = requests.get(self.__url, proxies = self.__proxy)
         soup = BeautifulSoup(resp.content, "html.parser")
         chListCols = soup.find_all("ul", attrs={"class": "nr6 lan2", "id": re.compile("^cbc_\d$")})
         no = 0
         for col in chListCols:
             oneCol = col.find_all("a", attrs={"class": "tg"})
             for ch in oneCol:
-                self.__chList.append((no, ch.get("href"), ch.get("title")))
+                sys.stdout.write(str(no)+" "+ch.get("href")+" "+ch.get("title")+"\n")
                 self.tasker.PutIntoChapterQueue(ChapterWork(no, ch.get("title"), ch.get("href").rstrip("/")))
                 no += 1
-
-    def PrintAllHref(self):
-        for a in self.__chList:
-            print a[0], a[1], a[2]
