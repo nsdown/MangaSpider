77f442be53cae3ee2012877c20ad83c58091aa49
Juice Yang <abandonclock@gmail.com>
2015-10-30 18:42:56 +0800

    correct requirement error

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name='MangaSpider',
     version='0.1.0',
-    packages=['reportlab','jsbeautifier','requests'],
+    packages=[''],
     url='',
     license='',
     author='Juiceyang',
     author_email='abandonclock@gmail.com',
-    description='Scrape manga from dm5.com'
+    description='Scrape manga from dm5.com', requires=['reportlab', 'requests', 'jsbeautifier']
 )
