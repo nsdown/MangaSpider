62bdd5068abe892667069392783327fc84ef2f4f
Juice Yang <abandonclock@gmail.com>
2015-10-30 18:39:52 +0800

    add setuptools script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='MangaSpider',
+    version='0.1.0',
+    packages=['reportlab','jsbeautifier','requests'],
+    url='',
+    license='',
+    author='Juiceyang',
+    author_email='abandonclock@gmail.com',
+    description='Scrape manga from dm5.com'
+)
